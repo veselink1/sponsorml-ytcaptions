c77b34dd1a12820436472f3949abaf926ec7b131
Veselin Karaganev <vesko.karaganev@gmail.com>
2022-04-14 15:16:58 +0100

    Ignore overlapping segments
    The code was based on code from SponsorBlock relevant to sponsor
    segments only. SponsorBlock also tries to randomize which segment is
    picked when there are overlapping segments to allow people to vote on
    them. This is not relevant to us and we pick the segment with the
    highest number of votes.
    
    Improved information in exception traces.

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,8 @@
 import os
+import sys
+from typing import List, Tuple
 import pandas as pd
+from enum import Enum
 
 class Caption:
 	def __init__(self, start: int, end: int, text: str):
@@ -10,9 +13,34 @@ class Caption:
 	def __repr__(self):
 		return self.text
 
+class DBSegment:
+	category: str
+	startTime: float
+	endTime: float
+	UUID: str
+	userID: str
+	votes: int
+	views: int
+	locked: int
+	hidden: int
+	shadowHidden: int
+	videoID: str
+	videoDuration: int
+	reputation: int
+	hashedVideoID: str
+	timeSubmitted: int
+	userAgent: str
+	service: str
+	description: str
+
+class OverlappingSegmentGroup:
+	def __init__(self):
+		self.segments: List[DBSegment] = []
+		self.votes = 0
+
 def get_caption_list_from_path(path: str) -> list[Caption]:
 	captions = []
-	with open(path) as f:
+	with open(path, encoding='utf-8') as f:
 		lines = f.readlines()
 
 		# remove double \n then remove \n
@@ -24,10 +52,14 @@ def get_caption_list_from_path(path: str) -> list[Caption]:
 
 		start = breakpoints[0] + 1
 		for end in breakpoints[1:]:
-			timestamps, text = lines[start], " ".join(lines[start+1:end])
+			timestamps_text, text = lines[start], " ".join(lines[start+1:end])
 			text = clean_text(text)
-			timestamps = timestamps.split(' ')
-			timestamps = [timestamps[0], timestamps[2]]
+			timestamps = timestamps_text.split(' ')
+			try:
+				timestamps = [timestamps[0], timestamps[2]]
+			except IndexError as e:
+				e.args = (*e.args, f'while parsing "{timestamps_text}"')
+				raise e
 
 			start_time, end_time = [_get_timestamp_in_seconds(ts) for ts in timestamps]
 
@@ -41,7 +73,7 @@ def _get_timestamp_in_seconds(timestamp: str) -> float:
 	return (h * 3600) + (m * 60) + s
 
 # allow an error margin for the caption to be considered part of the segment
-def get_intersection_range(captions: list[Caption], start: float, end: float, error: float = 0.2) -> (float, float):
+def get_intersection_range(captions: list[Caption], start: float, end: float, error: float = 0.2) -> Tuple[float, float]:
 	segment_range = [0, 0]
 	for i in range(len(captions)):
 		if (captions[i].start + error) >= start:
@@ -61,21 +93,76 @@ def clean_text(text: str) -> str:
 	# no cleaning yet
 	return text
 
-def prepare_data(captions_path: str, sponsorml_path: str, output_path: str = 'data.csv', vote_threshold: int = 20):
+def build_segment_groups(segments: List[DBSegment]) -> List[OverlappingSegmentGroup]:
+	"""
+	This function will find segments that are contained inside of eachother, called similar segments.
+	Segments with less than -1 votes are already ignored before this function is called.
+
+	Based on https://github.com/ajayyy/SponsorBlockServer/blob/e74b985304443b17b429c5c82696c7a03e78a166/src/routes/getSkipSegments.ts#L276
+	"""
+
+	# Create groups of segments that are similar to eachother
+	# Segments must be sorted by their startTime so that we can build groups chronologically:
+	# 1. As long as the segments' startTime fall inside the currentGroup, we keep adding them to that group
+	# 2. If a segment starts after the end of the currentGroup (> cursor), no other segment will ever fall
+	#    inside that group (because they're sorted) so we can create a new one
+	overlappingSegmentsGroups: List[OverlappingSegmentGroup] = []
+	currentGroup = None
+	cursor = -1 # -1 to make sure that, even if the 1st segment starts at 0, a new group is created
+	for segment in segments:
+		if segment.startTime >= cursor:
+			currentGroup = OverlappingSegmentGroup()
+			overlappingSegmentsGroups.append(currentGroup)
+
+		currentGroup.segments.append(segment)
+		# only if it is a positive vote, otherwise it is probably just a sponsor time with slightly wrong time
+		if segment.votes > 0:
+			currentGroup.votes += segment.votes
+
+		cursor = max(cursor, segment.endTime)
+
+	return overlappingSegmentsGroups
+
+def get_best_segment(group: OverlappingSegmentGroup):
+	"""
+	SponsorBlock chooses a segment from an overlap group randomly by using the
+	votes property as a weight. This is done so that all segments can have a
+	chance of appearing and makes sense in that system, but here we just
+	want the best possible match, hence we pick the segment with the highest vote.
+	"""
+
+	return max(group.segments, key=lambda segment: segment.votes)
+
+def prepare_data(captions_path: str, sponsorml_path: str, output_path: str, vote_threshold: int):
 	filenames = os.listdir(captions_path)
+	print('Reading database...')
 	sponsorml_df = pd.read_csv(sponsorml_path)
 	rows = []
 
-	for filename in filenames:
+	print('Processing captions...')
+	for i, filename in enumerate(filenames):
+		progress = (i + 1) / len(filenames) * 100
+		print(f'\r{progress:.2f}%', end='', flush=True)
 		videoID = filename.split('.')[0]
 		# get all labeled segments of video
 		segments = sponsorml_df[sponsorml_df.videoID == videoID]
-		for _, segment in segments.iterrows():
-			# only check sponsor segments and those with votes >= input threshold
-			if segment.category != 'sponsor' or segment.votes < vote_threshold:
-				continue
 
-			captions = get_caption_list_from_path(f'{captions_path}/{filename}')
+		if len(segments) == 0:
+			print(f'No segments for {videoID}!')
+
+		# segment filtering
+		# https://github.com/ajayyy/SponsorBlockServer/blob/e74b985304443b17b429c5c82696c7a03e78a166/src/routes/getSkipSegments.ts#L18
+		segments = [segment for _, segment in segments.iterrows()
+			if segment.category == 'sponsor' and segment.votes >= vote_threshold] # TODO: Think about adding a threshold on `views`
+		# Filter out similar segments
+		segments = [get_best_segment(group) for group in build_segment_groups(segments)]
+
+		for segment in segments:
+			try:
+				captions = get_caption_list_from_path(f'{captions_path}/{filename}')
+			except Exception as e:
+				e.args = (*e.args, f'while processing {filename}')
+				raise e
 
 			# extract transcript
 			full_transcript = " ".join([caption.text for caption in captions])
@@ -88,24 +175,26 @@ def prepare_data(captions_path: str, sponsorml_path: str, output_path: str = 'da
 			token_start = 0
 			for i in range(start_index):
 				token_start += len(tokenize(captions[i].text))
-			
+
 			token_end = token_start + 1
 			for i in range(start_index, end_index):
 				token_end += len(tokenize(captions[i].text))
-			
+
 			rows.append((videoID, full_transcript, sponsor_text, (token_start, token_end)))
 
 	df = pd.DataFrame(rows, columns=['videoID', 'transcript', 'sponsorText', 'sponsorTokenRange'])
-	df.to_csv('data.csv')
+	df.to_csv(output_path)
 
+def get_or_default(arr, i, default):
+	return arr[i] if i < len(arr) else default
 
-CAPTIONS_PATH = 'captions'
-SPONSORML_PATH = 'sponsorTimes.csv'
-OUTPUT_PATH = 'data.csv'
-VOTE_THRESHOLD = 20
+CAPTIONS_PATH = get_or_default(sys.argv, 1, 'captions')
+SPONSORML_PATH = get_or_default(sys.argv, 2, 'sponsorTimes.csv')
+OUTPUT_PATH = get_or_default(sys.argv, 3, 'data.csv')
+VOTE_THRESHOLD = -1 # Same threshold SponsorBlock is using
 
 def main():
 	prepare_data(CAPTIONS_PATH, SPONSORML_PATH, OUTPUT_PATH, VOTE_THRESHOLD)
-	
+
 if __name__ == '__main__':
 	main()
